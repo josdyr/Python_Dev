b924cb22df9190c10ed32a2c265663e6b6d5c9e8
Jostein Dyrseth <josdyr@Josteins-MacBook-Pro.local>
2017-10-04 13:49:41 +0100

    note updates

---
+++ python_notes.py
@@ -0,0 +1,16 @@
+# classes
+class Theif:
+    sneaky = True
+
+    def __init__(self, name, sneaky=True, **kwargs):
+        self.name = name
+        self.neaky = sneaky
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def pickpocket(self):
+        return self.sneaky and bool(random.randint(0, 1))
+
+    def hide(self, light_level):
+        return self.sneaky and light_level < 10
